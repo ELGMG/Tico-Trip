21eac0af9ea7d8013340c4769414f20243be11f5
Justin <justingvs@gmail.com>
2022-04-23 01:09:32 -0600

    ventaja de provincia

--- src/app/guanacaste/guanacaste.component.spec.ts
+++ src/app/guanacaste/guanacaste.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GuanacasteComponent } from './guanacaste.component';
 
+
+
 describe('GuanacasteComponent', () => {
   let component: GuanacasteComponent;
   let fixture: ComponentFixture<GuanacasteComponent>;
